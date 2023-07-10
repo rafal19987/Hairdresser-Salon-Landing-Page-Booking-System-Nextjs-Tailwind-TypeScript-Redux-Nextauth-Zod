a7430b38dd6c294dbc430ba1b4251495071f67be
rafal19987 <rafal987.1@o2.pl>
2023-07-10 21:32:06 +0200

    feat: create footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/shared-structures/header/Header';
+import Footer from '@/components/shared-structures/footer/Footer';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
 
@@ -21,13 +22,14 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <body
-        className={`${montserrat.className} flex flex-col items-center justify-between w-screen min-h-screen h-full bg-[var(--body-bg-dark)] text-[var(--primary-text-color)] font-normal`}
+        className={`${montserrat.className} flex flex-col items-center justify-between w-screen min-h-screen h-full bg-[var(--body-bg-dark)] text-[var(--primary-text-color)] font-normal overflow-x-hidden`}
       >
         <div className="flex flex-col items-center w-full max-w-[1600px] min-h-screen h-full">
           <Header />
           <main className="flex flex-col items-center grow w-full">
             {children}
           </main>
+          <Footer />
         </div>
       </body>
     </html>
