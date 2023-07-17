0a814a54a3bfd610b804290f9fe1544a845be2d8
rafal19987 <rafal987.1@o2.pl>
2023-07-17 11:16:33 +0200

    style: redesign footer

--- src/components/shared-structures/footer/Footer.tsx
+++ src/components/shared-structures/footer/Footer.tsx
@@ -6,37 +6,42 @@ import googleIcon from '@/assets/google-icon.svg';
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center w-full h-full py-4 px-6 border-t border-[var(--gold)] bg-black">
-      <ul className="flex flex-col items-end gap-5 w-full">
-        <li className="flex flex-row-reverse gap-3 items-start w-fit">
-          <Image
-            src={instagramIcon}
-            alt="instagram icon"
-            width={20}
-            height={20}
-          />
-          <Link className="text-sm" href={'#'}>
-            Odwiedź Nas na Instagramie
-          </Link>
-        </li>
-        <li className="flex flex-row-reverse gap-3 w-fit">
-          <Image
-            src={facebookIcon}
-            alt="facebook icon"
-            width={20}
-            height={20}
-          />
-          <Link className="text-sm" href={'#'}>
-            Polub Nas na Facebooku
-          </Link>
-        </li>
-        <li className="flex flex-row-reverse gap-3 items-start w-fit">
-          <Image src={googleIcon} alt="google icon" width={20} height={20} />
-          <Link className="text-sm" href={'#'}>
-            Wystaw opinię w Google
-          </Link>
-        </li>
-      </ul>
+    <footer
+      className="
+    flex items-center justify-center w-full  dark:border-t dark:border-[var(--gold)] bg-[var(--header-bg-light)] dark:bg-[var(--header-bg-dark)]"
+    >
+      <div className="flex items-center justify-between w-full p-4  lg:max-w-[1600px]">
+        <ul className="flex flex-col items-end gap-5 w-full h-full ">
+          <li className="flex flex-row-reverse gap-3 items-start w-fit">
+            <Image
+              src={instagramIcon}
+              alt="instagram icon"
+              width={20}
+              height={20}
+            />
+            <Link className="text-sm" href={'#'}>
+              Odwiedź Nas na Instagramie
+            </Link>
+          </li>
+          <li className="flex flex-row-reverse gap-3 w-fit">
+            <Image
+              src={facebookIcon}
+              alt="facebook icon"
+              width={20}
+              height={20}
+            />
+            <Link className="text-sm" href={'#'}>
+              Polub Nas na Facebooku
+            </Link>
+          </li>
+          <li className="flex flex-row-reverse gap-3 items-start w-fit">
+            <Image src={googleIcon} alt="google icon" width={20} height={20} />
+            <Link className="text-sm" href={'#'}>
+              Wystaw opinię w Google
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
