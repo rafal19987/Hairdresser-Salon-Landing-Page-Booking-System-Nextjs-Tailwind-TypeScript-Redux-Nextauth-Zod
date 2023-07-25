89a117b40125605e7c24f5731dcefd66a73e45ac
rafal19987 <rafal987.1@o2.pl>
2023-07-25 13:48:31 +0200

    feat: create table without react-table

--- src/components/pages/prices/PricesPage.tsx
+++ src/components/pages/prices/PricesPage.tsx
@@ -1,158 +1,212 @@
 'use client';
 
 import { useState } from 'react';
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from '@tanstack/react-table';
 
 type TService = {
+  id: number;
   service: string;
   price: string;
-  performer: string;
+  performer: string[];
 };
 
 const services: TService[] = [
   {
+    id: 1,
     service: 'Strzyżenie męskie - broda',
     price: '50.00 zł',
-    performer: 'Agnieszka',
+    performer: ['Agnieszka'],
   },
   {
+    id: 2,
     service: 'Strzyżenie męskie - bródka',
     price: '20.00 zł',
-    performer: 'Agnieszka',
+    performer: ['Agnieszka'],
   },
   {
+    id: 3,
     service: 'Strzyżenie męskie - klasyczne',
     price: '60.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 4,
     service: 'Strzyżenie męskie - klasyczne - włosy długie',
     price: '80.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 5,
     service: 'Strzyżenie męskie - klasyczne + broda',
     price: '95.00 zł',
-    performer: 'Agnieszka',
+    performer: ['Agnieszka'],
   },
   {
+    id: 6,
     service: 'Strzyżenie męskie - klasyczne + fade',
     price: '65.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 7,
     service: 'Strzyżenie męskie - klasyczne + skinfade',
     price: '70.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 8,
     service: 'Strzyżenie męskie - klasyczne + koloryzacja',
     price: '140.00 zł',
-    performer: 'Wiktoria',
+    performer: ['Wiktoria'],
   },
   {
+    id: 9,
     service: 'Strzyżenie męskie - maszynką - 0-3mm',
     price: '45.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 10,
     service: 'Strzyżenie męskie - maszynką - 6-18mm',
     price: '50.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 11,
     service: 'Strzyżenie męskie - wąs',
     price: '15.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 12,
     service: 'Woskowanie - Nos/Uszy',
     price: '15.00 zł',
-    performer: 'Wiktoria Agnieszka',
+    performer: ['Wiktoria', 'Agnieszka'],
   },
   {
+    id: 13,
     service: 'Strzyżenie dzieci do 4 lat',
     price: '50.00 zł',
-    performer: 'Wiktoria',
+    performer: ['Wiktoria'],
   },
-];
-
-const columnHelper = createColumnHelper<TService>();
-
-const columns = [
-  columnHelper.accessor((row) => row.service, {
-    id: 'service',
-    cell: (info) => <i>{info.getValue()}</i>,
-    header: () => <span>Usługa</span>,
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor('price', {
-    cell: (info) => info.renderValue(),
-    header: () => <span>Cena</span>,
-    footer: (info) => info.column.id,
-  }),
-
-  columnHelper.accessor('performer', {
-    cell: (info) => <i>{info.getValue()}</i>,
-    header: () => <span>Dostępne u</span>,
-  }),
 ];
 
 const PricesPage = () => {
-  const [data, setData] = useState(() => [...services]);
+  const [data, setData] = useState(services);
+  const [filteredData, setFilteredData] = useState(data);
+  const [date, setDate] = useState(() => new Date());
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+  const sortByPrice = () => {
+    const sortedData = [...filteredData];
+    sortedData.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    setFilteredData(sortedData);
+  };
+
+  let month = date.getMonth();
+  let thisMonth = null;
+  const months = [
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzień',
+  ];
+
+  switch (month) {
+    case 0:
+      thisMonth = months[0];
+      break;
+    case 6:
+      thisMonth = months[6];
+      break;
+    default:
+      console.log('Ssij');
+  }
 
   return (
-    <>
-      <div>Prices Page</div>
+    <div className="w-full h-full p-4 border">
       <div>
-        <table className="">
-          <thead className="w-full h-14 bg-slate-700 text-xl text-white">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    className="min-w-44 w-56 p-2 text-start hover:bg-slate-500 transition-colors duration-200"
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
+        <table className="w-full h-full">
+          <caption className="p-4 text-xl">
+            Tabela dostępnych usług w naszym salonie
+          </caption>
+          <thead>
+            <tr className="bg-slate-500 uppercase ">
+              <th className="p-4 text-start hover:cursor-default">Usługa</th>
+              <th
+                className="text-start hover:cursor-pointer hover:text-slate-700"
+                onClick={sortByPrice}
+              >
+                Cena
+              </th>
+              <th
+                className="text-start hover:cursor-pointer hover:text-slate-700"
+                onClick={() => {
+                  setFilteredData(data);
+                }}
+              >
+                Dostępne u
+              </th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
-            {table.getRowModel().rows.map((row) => (
+            {filteredData.map((service: TService) => (
               <tr
-                key={row.id}
-                className="bg-cyan-800 even:bg-cyan-900 hover:bg-cyan-700 transition-colors duration-200"
+                key={service.id}
+                className="p-4 bg-slate-600 even:bg-slate-700 hover:bg-slate-800"
               >
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} className="px-2 py-4">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
+                <td className="p-4 hover:cursor-default">{service.service}</td>
+                <td className="hover:cursor-default">{service.price}</td>
+                <td className="py-4">
+                  <ul className="flex flex-col gap-2">
+                    {service.performer.map((performer) => (
+                      // eslint-disable-next-line
+                      <li className="w-fit text-blue-300 hover:text-blue-500 hover:cursor-pointer">
+                        <span
+                          onClick={() => {
+                            setFilteredData(
+                              data.filter((d) => d.performer[0] === performer)
+                            );
+                          }}
+                        >
+                          {performer}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </td>
+                <td>
+                  <button
+                    className="h-12 px-2 bg-slate-900 rounded-lg text-blue-300 hover:text-blue-500 hover:cursor-pointer"
+                    onClick={() => {
+                      console.log(services[service.id - 1].service);
+                    }}
+                  >
+                    Zarezewuj
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
+          <tfoot>
+            {' '}
+            <tr>
+              <td className="py-2 px-5 border text-center" colSpan={4}>
+                Cennik aktualny na dzień {date.getDate()} {thisMonth}{' '}
+                {date.getFullYear()}
+              </td>
+            </tr>
+          </tfoot>
         </table>
       </div>
-    </>
+    </div>
   );
 };
 
