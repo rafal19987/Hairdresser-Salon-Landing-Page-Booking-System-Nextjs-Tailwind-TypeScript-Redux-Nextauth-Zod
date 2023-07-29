c39eb608dda0aaa6e526d00197bb29370e8213f7
rafal19987 <rafal987.1@o2.pl>
2023-07-29 22:39:04 +0200

    fix: change page component cuz error in deployment

--- src/app/(pages)/(protected)/book-visit/page.tsx
+++ src/app/(pages)/(protected)/book-visit/page.tsx
@@ -1,12 +1,4 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import SERVICES from '@/helpers/constants/services';
-import { TServiceProps } from '@/types/serviceTypes';
-
 const BookVisit = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-
   return <div>{children}</div>;
 };
 
