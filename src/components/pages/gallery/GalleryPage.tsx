df11dd331b80caf8460b521f427cf82c74d2b631
rafal19987 <rafal987.1@o2.pl>
2023-08-08 18:19:26 +0200

    feat: change galleryPage

--- src/components/pages/gallery/GalleryPage.tsx
+++ src/components/pages/gallery/GalleryPage.tsx
@@ -1,9 +1,32 @@
-import GalleryItem from './GalleryItem';
 import IMAGES from '@/helpers/constants/images';
+import GridContainer from '@/components/shared-atoms/GridContainer';
+import FlexContainer from '@/components/shared-atoms/FlexContainer';
+import SectionWrapper from '@/components/shared-atoms/SectionWrapper';
+import ArticleWrapper from '@/components/shared-atoms/ArticleWrapper';
+import ImageContainer from '@/components/shared-atoms/ImageContainer';
+import ImageItem from '@/components/shared-atoms/ImageItem';
+import * as Typography from '@/components/shared-atoms/typography/Typography';
 
 const GalleryPage = () => {
   return (
-    <div className="w-full px-4 py-12">
+    <SectionWrapper>
+      <Typography.H1 text="Zdjęcia" />
+      <div className="flex flex-row flex-wrap items-center justify-center gap-8 w-full">
+        {IMAGES.map((image) => (
+          <ImageContainer
+            key={image.id}
+            className="h-[400px] w-[500px]"
+            rounded="none"
+          >
+            <ImageItem imageAlt={image.alt} imageSrc={image.src} />
+          </ImageContainer>
+        ))}
+      </div>
+      {/* <GridContainer> */}
+      {/* </GridContainer> */}
+    </SectionWrapper>
+
+    /*  <div className="w-full px-4 py-12">
       <section className="w-full">
         <h2>Zdjecia</h2>
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-8 lg:gap-12 w-full">
@@ -16,7 +39,7 @@ const GalleryPage = () => {
           ))}
         </ul>
       </section>
-    </div>
+    </div> */
   );
 };
 
---
+++ src/components/shared-atoms/GridContainer.tsx
@@ -0,0 +1,13 @@
+type TGridContainerProps = {
+  children: React.ReactNode;
+};
+
+const GridContainer = ({ children }: TGridContainerProps) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 w-full">
+      {children}
+    </div>
+  );
+};
+
+export default GridContainer;
