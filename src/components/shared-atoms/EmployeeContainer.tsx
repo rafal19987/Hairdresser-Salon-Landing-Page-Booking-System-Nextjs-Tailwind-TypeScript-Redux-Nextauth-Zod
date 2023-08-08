c8ec21e092c13a6bc6f1c2ee01706587663920c6
rafal19987 <rafal987.1@o2.pl>
2023-08-08 18:20:10 +0200

    feat: create EmployeeContainer component

---
+++ src/components/shared-atoms/EmployeeContainer.tsx
@@ -0,0 +1,13 @@
+type TEmployeeContainerProps = {
+  children: React.ReactNode;
+};
+
+const EmployeeContainer = ({ children }: TEmployeeContainerProps) => {
+  return (
+    <div className="grid w-full gap-6 md:grid-cols-2 md:gap-4 md:grid md:justify-items-center  md:py-8 md:rounded-3xl md:mt-12 md:bg-gray-600/25  ">
+      {children}
+    </div>
+  );
+};
+
+export default EmployeeContainer;
