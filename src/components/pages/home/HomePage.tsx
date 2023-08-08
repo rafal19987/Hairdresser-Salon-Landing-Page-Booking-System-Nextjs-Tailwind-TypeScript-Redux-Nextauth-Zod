ad8526ad580fa4ec046917efcfdabdf2ad7242ef
rafal19987 <rafal987.1@o2.pl>
2023-08-08 18:20:49 +0200

    refactor: rewrite homePage

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import Card from './components/Card';
 import homePageContent from '@/helpers/content/pages/home/homeContent';
+import * as Typography from '@/components/shared-atoms/typography/Typography';
 
 const HomePage = () => {
   const { primaryHeader, secondaryHeader } = homePageContent;
@@ -7,12 +8,8 @@ const HomePage = () => {
   return (
     <div className="flex  flex-col gap-6 items-center  h-[calc(100vh-var(--header-height))] w-full p-4">
       <div className="flex flex-col gap-4 w-full">
-        <h2 className="text-black dark:text-white text-3xl text-center font-bold">
-          {primaryHeader}
-        </h2>
-        <h3 className="text-black dark:text-white text-2xl text-center font-bold">
-          {secondaryHeader}
-        </h3>
+        <Typography.H1 text="Fryzjer męski" />
+        <Typography.H2 text="Umów się na wizytę" />
       </div>
       <div className="flex justify-center w-full h-4/5 mb-20 lg:w-fit lg:mt-12">
         <Card />
