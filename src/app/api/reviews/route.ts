20f931eccc1b5293f49e8c889f4f704833ee45c6
Rafixx <rafal987.1@o2.pl>
2023-08-18 14:16:00 +0200

    refactor: replace fetching reviews by server actions to api route

---
+++ src/app/api/reviews/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { TDataFromApiProps, TReviewsProps } from '@/types/apiResponseTypes';
+
+export async function GET() {
+  const API_KEY: string = process.env.API_KEY!;
+  const placeID: string = 'ChIJBehxuF__IUcRODcWq63-6Uo';
+  const API_URL: string = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&language=pl&key=${API_KEY}`;
+
+  try {
+    const res = await fetch(API_URL, {
+      next: { revalidate: 3600 },
+      cache: 'no-cache',
+    });
+    const data: TDataFromApiProps = await res.json();
+    const reviews: TReviewsProps[] = data.result.reviews;
+    return NextResponse.json(reviews);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
