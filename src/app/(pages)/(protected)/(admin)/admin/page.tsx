daf54d0009f8fa5ce8760e07b202f2cd3bc3b641
Rafał Izdebski <rafal987.1@o2.pl>
2023-10-23 13:57:47 +0200

    feature/57-create-admin-dashboard #57 (#59)
    * chore: install supabase
    
    * feature: create supabase client
    
    * feat: create api route for sending messages and use it in contact form
    
    * fix: sending messages via contact form now working properly
    
    * chore: add new hostname patterns for images in next config
    
    * typo: delete question mark in route
    
    * fix: rewrite image loader / delete image loader hook
    
    * refactor: replace fetching reviews by server actions to api route
    
    * feat: add fetching all employes from database
    
    * feat: create sceleton for admin dashboard

---
+++ src/app/(pages)/(protected)/(admin)/admin/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useAppSelector, useAppDispatch } from '@/store/hooks';
+import { deleteAllReserations } from '@/store/features/reservations/reservationsSlice';
+
+const AdminDashboard = () => {
+  const dispatch = useAppDispatch();
+
+  const reservations = useAppSelector((state) => state.reservations);
+  const employees = useAppSelector((state) => state.employees);
+  const services = useAppSelector((state) => state.services);
+
+  return (
+    <div>
+      <ul className='flex flex-col gap-4'>
+        {reservations.map((reservation) => (
+          <li key={reservation.id} className='flex items-center justify-evenly'>
+            <span className='w-96 border'>
+              {
+                services.find((service) => service.id === reservation.serviceId)
+                  ?.title
+              }
+            </span>
+            <span>
+              {
+                employees.find(
+                  (employee) => employee.id === reservation.executorId
+                )?.name
+              }
+            </span>
+            <span>{reservation.reservationDate?.day}</span>
+            <span>{reservation.reservationDate?.hour}</span>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => dispatch(deleteAllReserations())}>
+        Usuń rezerwacje
+      </button>
+    </div>
+  );
+};
+
+export default AdminDashboard;
