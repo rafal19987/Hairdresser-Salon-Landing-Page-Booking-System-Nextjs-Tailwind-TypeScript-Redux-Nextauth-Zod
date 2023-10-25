ed1690a075f5aca2adbc00e1332e9949ec402742
Rafixx <rafal987.1@o2.pl>
2023-10-25 19:57:19 +0200

    feat: wrap protected layount into nextauth provider

--- src/app/(pages)/(protected)/layout.tsx
+++ src/app/(pages)/(protected)/layout.tsx
@@ -1,20 +1,23 @@
-import { type Metadata } from 'next';
+import { NextAuthProvider } from '@/app/NextAuthProvider';
 import { Toaster } from 'react-hot-toast';
+import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Męski fryzjer | Zarezerwuj wizytę',
   description: 'Strona rezerwacji wizyty dla salonu fryzierskiego',
 };
 
-export default function BookVisitLayout({
+export default async function BookVisitLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className='flex flex-col md:flex-row grow w-full h-screen bg-white text-black text-base lg:text-xl font-lato transition-all duration-300'>
-      <Toaster />
-      {children}
-    </main>
+    <NextAuthProvider>
+      <main className='flex flex-col md:flex-row grow w-full h-screen bg-white text-black text-base lg:text-xl font-lato transition-all duration-300'>
+        <Toaster />
+        {children}
+      </main>
+    </NextAuthProvider>
   );
 }
