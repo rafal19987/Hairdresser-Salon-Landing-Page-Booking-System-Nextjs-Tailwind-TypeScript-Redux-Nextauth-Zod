7f830edb1e1480cb7d9cb75ff668ed2ce6876145
Rafixx <rafal987.1@o2.pl>
2023-11-19 14:51:12 +0100

    feat: add Content Table and Modal panel for reservation

--- src/app/(pages)/(protected)/(user)/book/page.tsx
+++ src/app/(pages)/(protected)/(user)/book/page.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import { useSession, signOut, signIn } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { Modal } from '@/components/pages/book-visit/components/modal';
 import { Table } from './Table';
 
 const BookPage = () => {
   const { data: session } = useSession();
 
-  if (!session?.user) return <button onClick={() => signIn()}>Zaloguj</button>;
+  if (!session?.user) return null;
 
   return (
-    <div className='relative flex flex-col grow overflow-x-hidden border'>
-      {session && <button onClick={() => signOut()}>Wyloguj</button>}
-      <h1>{session?.user?.name}</h1>
+    <div className='relative flex flex-col grow overflow-x-hidden'>
       <section className=' overflow-y-auto overflow-x-hidden p-8'>
         <Table />
       </section>
