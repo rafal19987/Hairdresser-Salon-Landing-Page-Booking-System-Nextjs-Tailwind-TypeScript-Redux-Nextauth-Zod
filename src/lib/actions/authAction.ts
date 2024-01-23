abd51943f25bb4e06c3f24c0f111302d9ef8c66e
Rafixx <rafal987.1@o2.pl>
2024-01-23 19:55:19 +0100

    feat: create AuthAction for register user

---
+++ src/lib/actions/authAction.ts
@@ -0,0 +1,19 @@
+'use sever';
+
+// import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import prisma from '../prisma';
+
+export async function registerUser(
+  user: Omit<User, 'id' | 'emailVerified' | 'image'>,
+) {
+  const result = await prisma.user.create({ data: user });
+
+  // const jwtUserId = signJwt({
+  //   id: result.id,
+  // });
+  // const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${jwtUserId}`;
+  // const body = compileActivationTemplate(user.firstName, activationUrl);
+  // await sendMail({ to: user.email, subject: "Activate Your Account", body });
+  // return result;
+}
