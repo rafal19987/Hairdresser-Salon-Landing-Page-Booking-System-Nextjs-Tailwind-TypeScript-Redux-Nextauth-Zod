c7b9d58c41555ec224c74d580b438112685964f7
Rafixx <rafal987.1@o2.pl>
2024-01-24 21:18:45 +0100

    feat: crate type modules for JWT and Session

---
+++ src/lib/types.d.ts
@@ -0,0 +1,13 @@
+import { type User } from '@prisma/client';
+
+declare module 'next-auth' {
+  interface Session {
+    user: User;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    user: User;
+  }
+}
