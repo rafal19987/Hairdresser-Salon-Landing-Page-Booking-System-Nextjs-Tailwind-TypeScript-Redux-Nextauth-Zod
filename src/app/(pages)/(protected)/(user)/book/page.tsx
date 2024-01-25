35577fe8771aa22465bf2eba22ab299ff829a0e8
Rafixx <rafal987.1@o2.pl>
2024-01-25 15:47:01 +0100

    refactor: change book page from client to server side

--- src/app/(pages)/(protected)/(user)/book/page.tsx
+++ src/app/(pages)/(protected)/(user)/book/page.tsx
@@ -1,17 +1,20 @@
-'use client';
-
-import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { Modal } from '@/components/pages/book-visit/components/modal';
 import { Table } from './Table';
+import { Button } from '@/components/shared-atoms/Button';
+import { SigninButton } from '@/components/shared-atoms/SigninButton';
 
-const BookPage = () => {
-  const { data: session } = useSession();
+const BookPage = async () => {
+  const session = await getServerSession(authOptions);
 
-  if (!session?.user) return null;
+  if (!session?.user) redirect('/login');
 
   return (
     <div className='relative flex flex-col grow overflow-x-hidden'>
       <section className=' overflow-y-auto overflow-x-hidden p-8'>
+        <SigninButton />
         <Table />
       </section>
       <Modal />
