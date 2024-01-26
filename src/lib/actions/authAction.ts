29cad51401c46a9b34afe5a60cee349495864811
Rafixx <rafal987.1@o2.pl>
2024-01-26 14:55:36 +0100

    feat: create Jwt strategy with activation User server action func

--- src/lib/actions/authAction.ts
+++ src/lib/actions/authAction.ts
@@ -3,19 +3,64 @@
 import { User } from '@prisma/client';
 import prisma from '../prisma';
 import * as bcrypt from 'bcrypt';
+import { compileActivationTemplate, sendMail } from '../mail';
+import { signJwt, verifyJwt } from '../jwt';
 
 export async function registerUser(
   user: Omit<User, 'id' | 'emailVerified' | 'image'>,
 ) {
-  const result = await prisma.user.create({
-    data: { ...user, password: await bcrypt.hash(user.password, 10) },
+  try {
+    const result = await prisma.user.create({
+      data: { ...user, password: await bcrypt.hash(user.password, 10) },
+    });
+
+    const jwtUserId = signJwt({
+      id: result.id,
+    });
+
+    const activationUrl = `${process.env.HOST_URL}/activation/${jwtUserId}`;
+
+    const htmlBody = compileActivationTemplate(user.name, activationUrl);
+
+    await sendMail({
+      to: user.email,
+      subject: 'Aktywuj swoje konto',
+      body: htmlBody,
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+type ActivateUserFunc = (
+  jwtUserId: string,
+) => Promise<'userNotExist' | 'alreadyActivated' | 'success'>;
+
+export const activateUser: ActivateUserFunc = async (jwtUserId) => {
+  const payload = verifyJwt(jwtUserId);
+  const userId = payload?.id;
+
+  const user = await prisma.user?.findUnique({
+    where: {
+      id: userId,
+    },
   });
 
-  // const jwtUserId = signJwt({
-  //   id: result.id,
-  // });
-  // const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${jwtUserId}`;
-  // const body = compileActivationTemplate(user.firstName, activationUrl);
-  // await sendMail({ to: user.email, subject: "Activate Your Account", body });
-  // return result;
-}
+  if (!user) return 'userNotExist';
+
+  if (user.emailVerified) return 'alreadyActivated';
+
+  const result = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      emailVerified: new Date(),
+    },
+  });
+
+  return 'success';
+};
