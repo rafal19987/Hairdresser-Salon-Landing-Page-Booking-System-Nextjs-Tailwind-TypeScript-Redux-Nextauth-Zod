3bc3acb5e5f92b92ba95f7f88d931ae48c206ca1
Rafixx <rafal987.1@o2.pl>
2024-08-05 19:19:51 +0200

    refactor: gallery page

--- src/components/pages/gallery/GalleryPage.tsx
+++ src/components/pages/gallery/GalleryPage.tsx
@@ -1,24 +1,49 @@
+'use client';
+
+import { useState } from 'react';
 import IMAGES from '@/helpers/constants/images';
 import SectionWrapper from '@/components/shared-atoms/SectionWrapper';
-import ImageContainer from '@/components/shared-atoms/ImageContainer';
-import ImageItem from '@/components/shared-atoms/ImageItem';
 import * as Typography from '@/components/shared-atoms/typography/Typography';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const GalleryPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <SectionWrapper>
       <Typography.H1 text='Galeria' />
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-        {IMAGES.map((image) => (
+        {IMAGES.map((image, idx) => (
           <Image
-            className='h-full bg-neutral-900'
+            key={idx}
+            className={`h-full bg-neutral-900 transition-all duration-500 ${
+              isLoading
+                ? 'scale-105 blur-sm grayscale animate-pulse'
+                : 'scale-100 blur-0 grayscale-0'
+            }`}
             alt={image.alt}
             src={image.src}
-            placeholder='blur'
+            width={0}
+            height={0}
+            sizes='100vw'
+            style={{ width: '100%', height: '100%' }}
+            aria-hidden
+            onLoadingComplete={() => setIsLoading(false)}
           />
         ))}
       </div>
+      <span className='text-neutral-500'>
+        Zdjęcia pobrane z portalu{' '}
+        <Link
+          className='underline underline-offset-2 hover:text-neutral-400'
+          href='https://unsplash.com/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          unsplash
+        </Link>
+      </span>
     </SectionWrapper>
   );
 };
