6ca89f3f23a3123a97ea8b92c1754a23b43da1c0
Rafixx <rafal987.1@o2.pl>
2024-08-08 19:56:25 +0200

    feat: add animations to main pages

--- src/components/pages/contact/ContactPage.tsx
+++ src/components/pages/contact/ContactPage.tsx
@@ -11,34 +11,44 @@ import employee2 from '@/assets/employee-2-face-image-big.avif';
 const ContactPage = () => {
   return (
     <SectionWrapper>
-      <Typography.H1 text='Kontakt' />
+      <div data-aos='fade-down' data-aos-once='true'>
+        <Typography.H1 text='Kontakt' />
+      </div>
       <ArticleWrapper>
         <FlexContainer gap='8'>
           <FlexContainer gap='2'>
-            <Typography.H2 text='Godziny otwarcia:' align='center' />
-            <Typography.H3 text='Pon-PT 8:00 - 18:00' />
-            <Typography.H3 text='Sobota 7:00 - 20:00' />
+            <div data-aos='fade-down' data-aos-once='true' data-aos-delay='100'>
+              <Typography.H2 text='Godziny otwarcia:' align='center' />
+              <Typography.H3 text='Pon-PT 8:00 - 18:00' />
+              <Typography.H3 text='Sobota 7:00 - 20:00' />
+            </div>
           </FlexContainer>
         </FlexContainer>
       </ArticleWrapper>
       <ArticleWrapper>
         <EmployeeContainer>
-          <EmployeeCard
-            blockqoute='500 501 502'
-            imageAlt='Wiktoria'
-            photo={employee1}
-            imageCaption='Wiktoria'
-          />
-          <EmployeeCard
-            blockqoute='503 504 505'
-            imageAlt='Agnieszka'
-            photo={employee2}
-            imageCaption='Agnieszka'
-          />
+          <div data-aos='fade-right' data-aos-once='true' data-aos-delay='100'>
+            <EmployeeCard
+              blockqoute='500 501 502'
+              imageAlt='Wiktoria'
+              photo={employee1}
+              imageCaption='Wiktoria'
+            />
+          </div>
+          <div data-aos='fade-left' data-aos-once='true' data-aos-delay='100'>
+            <EmployeeCard
+              blockqoute='503 504 505'
+              imageAlt='Agnieszka'
+              photo={employee2}
+              imageCaption='Agnieszka'
+            />
+          </div>
         </EmployeeContainer>
       </ArticleWrapper>
       <ArticleWrapper>
-        <Typography.H2 text='Wyślij zapytanie' align='center' />
+        <div data-aos='fade-down' data-aos-once='true' data-aos-delay='100'>
+          <Typography.H2 text='Wyślij zapytanie' align='center' />
+        </div>
         <div className='mt-12 mb-12 w-full lg:flex lg:justify-center'>
           <ContactForm />
         </div>
--- src/components/pages/contact/componenets/contactForm/Label.tsx
+++ src/components/pages/contact/componenets/contactForm/Label.tsx
@@ -1,6 +1,12 @@
 const Label = ({ htmlFor, value }: { htmlFor: string; value: string }) => {
   return (
-    <label className='text-xl text-neutral-300' htmlFor={htmlFor}>
+    <label
+      className='text-xl text-neutral-300'
+      htmlFor={htmlFor}
+      data-aos='fade-down'
+      data-aos-once='true'
+      data-aos-delay='100'
+    >
       {value}:
     </label>
   );
